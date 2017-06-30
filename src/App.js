6ef7ef19b102d189d7252e8e8453172d75e87abf
Qiang (Glenn) Ji <qiang.ji@ge.com>
2017-06-30 16:18:05 +1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="hero is-primary">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">
+              Victorian Road Crashes Analysis
+            </h1>
+            <h2 className="subtitle">
+              2012 - 2017
+            </h2>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
