82dbf4f884adc985cf830d7e14039c775ce4dc5d
Qiang (Glenn) Ji <qiang.ji@ge.com>
2017-06-30 17:25:12 +1000

    component should return single element

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="hero is-primary">
-        <div className="hero-body">
-          <div className="container">
-            <h1 className="title">
-              Victorian Road Crashes Analysis
-            </h1>
-            <h2 className="subtitle">
-              2012 - 2017
-            </h2>
+      <div>
+        <div className="hero is-primary">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">
+                Victorian Road Crashes Analysis
+              </h1>
+              <h2 className="subtitle">
+                2012 - 2017
+              </h2>
+            </div>
           </div>
         </div>
       </div>
